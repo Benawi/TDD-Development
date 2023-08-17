1aee3e3324e5bb0831bbac9a000cfe3b7e7b8bcf
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-17 13:06:40 +0300

    returns an array

--- solver_spec.rb
+++ solver_spec.rb
@@ -9,4 +9,9 @@ RSpec.describe "FizzBuzz" do
     expect(fizzbuzz).to receive(:call).and_return([1, 2, "Fizz"])
     expect(fizzbuzz.call).to be_a(Proc)
   end
+  
+  it "returns an array" do
+    expect(fizzbuzz).to receive(:call).and_return([1, 2, "Fizz"])
+    expect(fizzbuzz.call).to be_an(Array) 
+  end
 end
