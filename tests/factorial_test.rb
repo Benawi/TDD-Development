42d13775ba2ab1528caced11ce92daff995630ff
ProgramKingAbel <abelmorara254@gmail.com>
2023-08-17 13:13:50 +0300

    use allow & receive methods return=>pretermined outputs for specific values

--- tests/factorial_test.rb
+++ tests/factorial_test.rb
@@ -2,5 +2,13 @@ require 'rspec'
 
 RSpec.describe Solver do
   let(:solver) { Solver.new}
+
+  describe '#factorial' do
+    context 'When factorial-like method is mocked' do
+      before do
+        allow(solver).to receive(:factorial).with(0).and_return(1)
+      end
+    end
+  end
 end
 
