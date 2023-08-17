faeb904366f863764a615804628d110871bdfe73
ProgramKingAbel <abelmorara254@gmail.com>
2023-08-17 13:14:47 +0300

    Add more test cases

--- tests/factorial_test.rb
+++ tests/factorial_test.rb
@@ -7,6 +7,9 @@ RSpec.describe Solver do
     context 'When factorial-like method is mocked' do
       before do
         allow(solver).to receive(:factorial).with(0).and_return(1)
+        allow(solver).to receive(:factorial).with(1).and_return(1)
+        allow(solver).to receive(:factorial).with(2).and_return(2)
+        allow(solver).to receive(:factorial).with(3).and_return(6)
       end
     end
   end
