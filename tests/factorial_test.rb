dc00d1e149e709afb9e2043cf948de846033daf8
ProgramKingAbel <abelmorara254@gmail.com>
2023-08-17 13:33:17 +0300

    Define test cases for factorial function

--- tests/factorial_test.rb
+++ tests/factorial_test.rb
@@ -1,16 +1,20 @@
+require_relative '../solver'
 require 'rspec'
 
 RSpec.describe Solver do
-  let(:solver) { Solver.new}
+  let(:solver) { Solver.new }
 
   describe '#factorial' do
-    context 'When factorial-like method is mocked' do
-      before do
-        allow(solver).to receive(:factorial).with(0).and_return(1)
-        allow(solver).to receive(:factorial).with(1).and_return(1)
-        allow(solver).to receive(:factorial).with(2).and_return(2)
-        allow(solver).to receive(:factorial).with(3).and_return(6)
-      end
+    it 'returns 1 for factorial of 0' do
+      expect(solver.factorial(0)).to eq(1)
+    end
+
+    it 'returns the correct factorial for positive numbers' do
+      expect(solver.factorial(5)).to eq(120)
+    end
+
+    it 'raises an exception for negative numbers' do
+      expect { solver.factorial(-2) }.to raise_error(RuntimeError, 'Factorial is not defined for negative numbers')
     end
   end
 end
