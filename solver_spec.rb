f3ff62f03dbf846b8e1e91140c4604ce8386b48a
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-17 13:06:12 +0300

    returns a function

--- solver_spec.rb
+++ solver_spec.rb
@@ -4,4 +4,9 @@ require 'mock'
 RSpec.describe "FizzBuzz" do
 
   let(:fizzbuzz) { mock() }
+
+  it "returns a function" do
+    expect(fizzbuzz).to receive(:call).and_return([1, 2, "Fizz"])
+    expect(fizzbuzz.call).to be_a(Proc)
+  end
 end
