bb4eb193a8879f1c6f892f296694e42e8fbe345d
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-17 13:07:35 +0300

    returns Buzz when divisible by 5

--- solver_spec.rb
+++ solver_spec.rb
@@ -19,4 +19,9 @@ RSpec.describe "FizzBuzz" do
     expect(fizzbuzz).to receive(:call).and_return([1, 2, "Fizz"])
     expect(fizzbuzz.call[2]).to eq("Fizz")
   end
+
+  it "returns Buzz when divisible by 5" do
+    expect(fizzbuzz).to receive(:call).and_return([1, 97, 98, "Buzz"])
+    expect(fizzbuzz.call[3]).to eq("Buzz")
+  end
 end
