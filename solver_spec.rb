7ce8eb6d602a1ac8d68d7f2fef4f72709f6255df
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-17 13:31:41 +0300

    fix linter error

--- solver_spec.rb
+++ solver_spec.rb
@@ -1,36 +1,35 @@
 require 'rspec'
-require 'mock'
 
 RSpec.describe 'FizzBuzz' do
-  let(:fizzbuzz) { mock }
+  let(:fizzbuzz) { double }
 
   it 'returns a function' do
-    expect(fizzbuzz).to receive(:call).and_return([1, 2, 'Fizz'])
+    allow(fizzbuzz).to receive(:call).and_return([1, 2, 'Fizz'])
     expect(fizzbuzz.call).to be_a(Proc)
   end
 
   it 'returns an array' do
-    expect(fizzbuzz).to receive(:call).and_return([1, 2, 'Fizz'])
+    allow(fizzbuzz).to receive(:call).and_return([1, 2, 'Fizz'])
     expect(fizzbuzz.call).to be_an(Array)
   end
 
   it 'returns Fizz when divisible by 3' do
-    expect(fizzbuzz).to receive(:call).and_return([1, 2, 'Fizz'])
+    allow(fizzbuzz).to receive(:call).and_return([1, 2, 'Fizz'])
     expect(fizzbuzz.call[2]).to eq('Fizz')
   end
 
   it 'returns Buzz when divisible by 5' do
-    expect(fizzbuzz).to receive(:call).and_return([1, 97, 98, 'Buzz'])
+    allow(fizzbuzz).to receive(:call).and_return([1, 97, 98, 'Buzz'])
     expect(fizzbuzz.call[3]).to eq('Buzz')
   end
 
   it 'returns FizzBuzz when divisible by 3 and 5' do
-    expect(fizzbuzz).to receive(:call).and_return([1, 2, 'Fizz', 4, 'Buzz', 'FizzBuzz'])
+    allow(fizzbuzz).to receive(:call).and_return([1, 2, 'Fizz', 4, 'Buzz', 'FizzBuzz'])
     expect(fizzbuzz.call[5]).to eq('FizzBuzz')
   end
 
   it 'returns the number when not divisible by 3 or 5' do
-    expect(fizzbuzz).to receive(:call).and_return([1, 2, 'Fizz', 13])
+    allow(fizzbuzz).to receive(:call).and_return([1, 2, 'Fizz', 13])
     expect(fizzbuzz.call[3]).to eq(13)
   end
 end
